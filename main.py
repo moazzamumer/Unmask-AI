e25bb329bb35738ba313ae7ff207175b60b37987
moazzamumer <moazzamumar22@gmail.com>
2025-07-28 00:41:37 +0500

    host

--- main.py
+++ main.py
@@ -28,4 +28,4 @@ def read_root():
 
 # Allow running via `python main.py`
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
